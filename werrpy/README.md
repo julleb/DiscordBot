af56e9c2e1e856e386708988b30de61b76b62e5f
dacechavez <dacechavez@gmail.com>
2018-01-10 21:36:25 +0100

    Added py implementation (github/lilpea/simplesoundbot)

---
+++ werrpy/README.md
@@ -0,0 +1,4 @@
+apt-get install python3-pip
+python3 -m pip install -U discord.py[voice]
+python3 firsttime.py
+python3 bot.py
---
+++ werrpy/bot.py
@@ -0,0 +1,64 @@
+''' The main file for the bot '''
+import os
+import sys
+import json
+import discord
+from discord import opus
+
+with open('bot.json') as data:
+    conf = json.load(data)
+
+client = discord.Client(max_messages=100)
+
+invoker = conf['invoker']
+
+
+@client.event
+async def on_ready():
+    ''' Executed when the bot successfully connects to Discord. '''
+    print('Logged in!\nName: {}\nId: {}'.format(
+        client.user.name, client.user.id))
+    if sys.maxsize > 2**32:
+        opus.load_opus('/usr/lib/x86_64-linux-gnu/libopus.so')
+    else:
+        opus.load_opus('libopus-0.x86.dll')
+
+
+@client.event
+async def on_message(msg):
+    ''' Executed when a message is posted to a server '''
+    if msg.author.id != client.user.id:  # ignore our own commands
+        if msg.content.startswith(conf['invoker']):
+            g_1 = msg.content.lower()[len(conf['invoker']):]
+            if g_1 == 'help':
+                g_4 = '\n' + conf['invoker']
+                g_3 = '{0}git\n{0}{1}'.format(
+                    conf['invoker'], g_4.join(os.listdir('sound/')))
+                g_3 = g_3.lower().replace(conf['fileformat'], '')
+                g_2 = 'Commands for ' + conf['bot'] + ':\n' + \
+                    g_3 + '\nYou must be in a voice channel for this to work.'
+                if not msg.channel.is_private:
+                    await client.send_message(msg.channel, 'Sending a DM of commands...')
+                await client.send_message(msg.author, g_2)
+            elif g_1 == 'git':
+                await client.send_message(msg.channel, 'https://github.com/lilpea/simplesoundbot')
+            else:
+                if msg.author.voice_channel:
+                    try:
+                        voice = await client.join_voice_channel(msg.author.voice_channel)
+                        player = voice.create_ffmpeg_player(
+                            'sound/' + g_1 + conf['fileformat'])
+                        player.start()
+                    except:
+                        pass
+                else:
+                    await client.send_message(msg.channel, 'You\'re not in a voice channel!')
+                while True:
+                    try:
+                        if player.is_done():
+                            await voice.disconnect()
+                            break
+                    except:
+                        break
+
+client.run(conf['token'])
